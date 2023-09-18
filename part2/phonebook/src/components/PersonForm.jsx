7d2e6f8b85cbe01b65f6bb4656f808926fdb49c4
Viktoriia <beloborodova.viktoriia@gmail.com>
2023-09-18 12:46:18 +0300

    added 2.16-2.17

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -1,29 +1,65 @@
+import phonebookService from "../services/phonebook"
+
 const PersonInput = ({ text, value, handleChange }) => {
   return (
     <div>{text}<input value={value} onChange={handleChange} /></div>
   )
 }
 
-const PersonForm = ({ persons,newName,newNumber,handleNameChange,handleNumberChange,setPersons,setNewName,setNewNumber}) => {
+const PersonForm = ({ persons,newName,newNumber,handleNameChange,handleNumberChange,setPersons,setNewName,setNewNumber,setMessage,setErrorMessage}) => {
   const addPerson = (event) => {
     event.preventDefault()
     if (checkPerson(newName)) {
-      alert(`${newName} is already added to phonebook`)
-    } else {
-      const personObject = {
-        name: newName,
-        id: persons.length + 1,
-        number: newNumber
-      }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      /*alert(`${newName} is already added to phonebook`)*/
+      /* Case when there is the name in the list */
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const personObject = {
+          name: newName,
+          number: newNumber
+        }
+        phonebookService
+          .update(checkPerson(newName), personObject)
+          .then(response => {
+            setPersons(persons.map(n => n.id !== checkPerson(newName) ? n : response.data))
+            setNewName('')
+            setNewNumber('')
+            setMessage(`${newName}'s number changed to ${newNumber}`)
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+          })
+          .catch(error => {
+            setErrorMessage(
+              `Information of ${newName} has already been removed from server`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            setPersons(persons.filter(person => person.name !== newName))
+          })
+        }
+      } else {
+        const personObject = {
+          name: newName,
+          number: newNumber
+        }
+        phonebookService
+        .create(personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+          setMessage(`Added ${newName}`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
     }
   }
 
   const checkPerson = (name) => {
     if (persons.find(x => x.name === name)) {
-      return true
+      return (persons.find(x => x.name === name)).id
     } else {
       return false
     }
