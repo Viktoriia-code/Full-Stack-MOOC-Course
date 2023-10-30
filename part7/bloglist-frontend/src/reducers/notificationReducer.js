a0367f4d1c098e8e49ca860e6beea4e8a6951ee3
Viktoriia <beloborodova.viktoriia@gmail.com>
2023-10-30 23:23:16 +0200

    init 7.10

---
+++ part7/bloglist-frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = null
+
+const notificationReducer = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    setNotification: (state, action) => {
+      return action.payload
+    },
+    removeNotification: (state) => {
+      return null
+    },
+  },
+})
+
+export const { setNotification, removeNotification } = notificationReducer.actions
+export default notificationReducer.reducer
