712d9348209fc234785c05a625d2fe7e178a9327
Viktoriia <beloborodova.viktoriia@gmail.com>
2023-11-14 22:55:53 +0200

    inited 8.13

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -97,6 +97,24 @@ let books = [
 /*
   you can remove the placeholder query once your first one has been implemented 
 */
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const Book = require('./models/book')
+const Author = require('./models/author')
+
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 const typeDefs = `
   type Book {
